7359590dae78bce8bff8cb1f1e28b5f616d4c46d
Kiril Dobrev <65949166+NetPriest@users.noreply.github.com>
2022-06-14 22:21:59 +0300

    Update converterUSD_BGN.java

--- javaExercises_2022/src/simpleEasyProblems/converterUSD_BGN.java
+++ javaExercises_2022/src/simpleEasyProblems/converterUSD_BGN.java
@@ -2,17 +2,23 @@ package simpleEasyProblems;
 
 import java.util.Scanner;
 
-/*9.	 1 USD = 1.79549 BGN.*/
+/*9.	USD to BGN converter 1 USD = 1.79549 BGN.*/
 
 public class converterUSD_BGN {
 
 	public static void main(String[] args) {
+		
 		Scanner keyScanner = new Scanner(System.in);
 		System.out.print("Please, enter the number of USD to convert to BGN: ");
+		//Read from keyboard
 		double currencyUSD = keyScanner.nextDouble();
+		
+		//Calculate
 		final double singleUSD = 1.79549;
 		double converted = currencyUSD * singleUSD;
 		System.out.print(currencyUSD + " USD are ");
+		
+		//Round till the second number after decimal .
 		System.out.printf("%.2f", converted);
 		System.out.print(" BGN.");
 	}
